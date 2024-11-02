1f295df4a1a5d95760b19bd5e750a418fa693d0e
Denys Astafiev <denis2004ast@gmail.com>
2024-11-02 17:13:22 +0200

    feat: enable cors and swagger description docs

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,27 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BigIntInterceptor } from './bigint.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+
+	const config = new DocumentBuilder()
+		.setTitle('API',)
+		.setDescription('Description',)
+		.setVersion('1.0',)
+		.build()
+  
+	const document = SwaggerModule.createDocument(app, config,)
+	SwaggerModule.setup('docs', app, document,)
+
+	app.enableCors({
+		origin:      [process.env.FRONTEND_REDIRECT_URL,],
+		credentials: true,
+	},)
+  
   app.useGlobalInterceptors(new BigIntInterceptor());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
