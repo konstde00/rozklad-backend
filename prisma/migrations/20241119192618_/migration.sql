75ac7fe564d42c019fd45ff713f104013b962709
kostiantyn-dementiev-op <kostiantyn.dementiev@observepoint.com>
2024-11-19 21:26:42 +0200

    Removed unnecessary `study_year` from StudentGroup

---
+++ prisma/migrations/20241119192618_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `study_year` on the `StudentGroup` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `StudentGroup` DROP COLUMN `study_year`;
